bcaf10b3f774bd78f25c07f585630d76e371e67b
Benni Dietz <b_diet05@uni-muenster.de>
2021-03-29 11:32:22 +0200

    dreamplaces

--- dreamplaces/script.js
+++ dreamplaces/script.js
@@ -1,10 +1,11 @@
-var url = "https://spreadsheets.google.com/feeds/cells/1oe7koTNUzTmhl9Z_133RRdQo-W01K9X9BxMvCFZydVw/od6/public/basic?alt=json";
+var url = "https://spreadsheets.google.com/feeds/cells/1Nc36on2CHcVC3nxxgXdlL4PrRnxCCRECXFHkHoxs8Vs/od6/public/basic?alt=json";
 var places = null
 
-var you_are_here_url = "you-are-here.png"
-
-var small_icon_size = 35
 var large_icon_size = 50
+
+var searchParams = new URLSearchParams(window.location.search)
+
+var selectedPlaceIndex = parseInt(searchParams.get("place"))-2
 
 class Category {
     constructor(name_in_table, name_to_display, icon_path, iconSize = large_icon_size) {
@@ -15,8 +16,11 @@ class Category {
         this.iconSize = iconSize;
     }
     
-    addPlace(place, html) {
-        L.marker(place.latlng.asArray(), {icon: getIcon(this.icon_path, this.iconSize)}).addTo(this.layerGroup.addTo(mymap)).bindPopup(html);   
+    addPlace(place, html, showPopup = false) {
+        var m = L.marker(place.latlng.asArray(), {icon: getIcon(this.icon_path, this.iconSize)}).addTo(this.layerGroup.addTo(mymap)).bindPopup(html);
+        if (showPopup) {
+            m.openPopup()
+        }
     }
 }
 
@@ -25,17 +29,9 @@ var categories = []
 categories.push(
     new Category("Stadt", "Stadt", "city.png", 60)
 )
+
 categories.push(
-    new Category("Freizeit", "Freizeit", "leisure_icon.png")
-)
-categories.push(
-    new Category("Shopping/Einkaufen", "Shopping", "shopping.png")
-)
-categories.push(
-    new Category("Essen", "Essen", "essen.png")
-)
-categories.push(
-    new Category("Gesundheit", "Gesundheit", "gesundheit.png")
+    new Category("Natur", "Natur", "berge.png", 60)
 )
 
 class LatLng {
@@ -50,7 +46,7 @@ class LatLng {
 }
 
 class PlaceToSee {
-    constructor(category, name, latlng, description, imageUrl, webseite) {
+    constructor(category, name, latlng, description, imageUrl,webseite) {
         this.category = category;
         this.name = name;
         this.latlng = latlng;
@@ -94,17 +90,11 @@ function retrievePlacesToSee(json) {
     return places
 }
 
-var overlayMaps = {}
-
 $.ajax({
     url:url,
     dataType:"jsonp",
     success:function(data) {
-        var freizeit_layer = L.layerGroup();
-        var stadt_layer = L.layerGroup();
-        var essen_layer = L.layerGroup();
-        var shopping_layer = L.layerGroup();
-        
+        var mensa_marker = L.layerGroup();
         places = retrievePlacesToSee(data)
         for (i in places) {
             place = places[i]
@@ -120,7 +110,10 @@ $.ajax({
             }
             if (place.latLng !== null) {
                 if (place.category != null) {
-                    place.category.addPlace(place, html)
+                    place.category.addPlace(place, html, i == selectedPlaceIndex)
+                    if (i == selectedPlaceIndex) {
+                        mymap.setView([place.latlng.lat, place.latlng.lng], 14)
+                    }
                 }
             }
         }
@@ -147,40 +140,10 @@ function getIcon(url, size) {
 var mymap = L.map('mapid', { 
     zoomControl: false,
     fullscreenControl: true
-}).setView([54.05128792423013, 10.749693008609134], 13);
+}).setView([51.96, 7.59], 4);
 var basemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoiYmVubmlkaWV0ejE5OTciLCJhIjoiY2p2YjQxOXVmMTR1YTQzcGs4am55anFsZSJ9.AMVgZjCNNILvwXnLS3DbyA'
 }).addTo(mymap);
-
-// mymap.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
-//         .on('locationfound', function(e){
-//             var marker = L.marker([e.latitude, e.longitude], {icon: getIcon(you_are_here_url, large_icon_size)} );
-//             var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
-//                 weight: 1,
-//                 color: 'blue',
-//                 fillColor: '#cacaca',
-//                 fillOpacity: 0.2
-//             });
-//             mymap.addLayer(marker).openPopup();
-//             mymap.addLayer(circle);
-//             mymap.panTo(new L.LatLng(e.latitude, e.longitude));
-//         })
-//        .on('locationerror', function(e){
-//             console.log(e);
-//             alert("Location access denied.");
-//         });
-
-// mymap.on('zoomend', function() {
-//     var currentZoom = mymap.getZoom();
-//     if (currentZoom < 11) {
-//         overlayMaps.eachLayer(function(layer) {
-//             //if (layer.feature.properties.num < 0.5)
-//             return layer.setIcon(getIcon(essen_icon_url, small_icon_size));
-//             //else if (feature.properties.num < 1.0)
-//               //  return layer.setIcon(ar_icon_2_double_size);
-//         });
-//     }
-// });
